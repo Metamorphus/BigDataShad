7547222dd7ff8563eeb759609d5d326364431901
Igor <hospital-at-home@yandex.ru>
2017-10-06 01:01:43 +0300

    bla

--- hw1/group1/run.sh
+++ hw1/group1/run.sh
@@ -3,7 +3,7 @@ yarn jar /opt/hadoop/hadoop-streaming.jar \
 -files mapper1.py,reducer1.py \
 -mapper "mapper1.py" \
 -reducer "reducer1.py" \
--input /user/sandello/logs/access.log.$1 \
+-input /user/bigdatashad/logs/access.log.$1 \
 -output /user/imorozov/hw1/group1/$1-temp \
 -numReduceTasks 10
 
